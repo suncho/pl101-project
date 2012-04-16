43e6cfd3d3749908e75dc686fb059a3fd9dbd3fa
Alex Howlett <alex@suncho.com>
2012-04-16 10:42:03 -0400

    Added repeats.

--- mus/compiler.js
+++ mus/compiler.js
@@ -12,21 +12,32 @@ var compile = function (musexpr) {
     var time_l, time_r;
 
     switch (expr.tag) {
-      case 'seq':
-        time = compile_inner (time, expr.left);
-        time = compile_inner (time, expr.right);
+      // Process a single note.
+      case 'note':
+        compiled.push ( 
+          { tag: 'note', pitch: pitch2midi(expr.pitch), start: time, dur: expr.dur }
+        );
+        time += expr.dur;
         break;
+      // Process two mus expressions concurrently.
       case 'par':
         time_l = compile_inner (time, expr.left);
         time_r = compile_inner (time, expr.right);
         time = Math.max (time_l, time_r);
         break;
-      case 'note':
-        compiled.push ( 
-          { tag: 'note', pitch: pitch2midi(expr.pitch), start: time, dur: expr.dur }
-        );
+      // Repeat an expression 'count' times.
+      case 'repeat':
+        for (i = expr.count; i > 0; --i)
+          time = compile_inner (time, expr.section);
+        break;
+      // Play nothing for a duration.
       case 'rest':
         time += expr.dur;
+        break;
+      // Process two mus expressions in sequence.
+      case 'seq':
+        time = compile_inner (time, expr.left);
+        time = compile_inner (time, expr.right);
         break;
     }
     return time;
@@ -38,16 +49,18 @@ var compile = function (musexpr) {
 var melody_mus = 
   { tag: 'seq',
     left:
-      { tag: 'seq',
-        left: { tag: 'note', pitch: 'a4', dur: 250 },
-        right:
-          { tag: 'seq',
-            left: { tag: 'rest', dur: 100 },
-            right: { tag: 'note', pitch: 'b4', dur: 250 } } },
+    { tag: 'seq',
+      left: { tag: 'note', pitch: 'a4', dur: 250 },
+      right:
+        { tag: 'seq',
+          left: { tag: 'rest', dur: 100 },
+          right: { tag: 'note', pitch: 'b4', dur: 250 } } 
+    },
     right:
-      { tag: 'seq',
-        left: { tag: 'note', pitch: 'c4', dur: 500 },
-        right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+    { tag: 'repeat',
+      section: { tag: 'note', pitch: 'c5', dur: 251 }, 
+      count: 6 }
+  };
 
 console.log (melody_mus);
 console.log (compile(melody_mus));
